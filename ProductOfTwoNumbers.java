aa6127ebaf9d0970028f2462fcfadfb80b67b636
Smontesosorio <e5smontes1@berchmans.edu.co>
2025-09-29 08:25:46 -0500

    Update ProductOfTwoNumbers.java

--- ProductOfTwoNumbers.java
+++ ProductOfTwoNumbers.java
@@ -1,1 +1,45 @@
+//Student's name: Santiago Montes
+//Group: 10B
+//Date: 22/09/2025
+//Topic: Java
 
+import java.util.Scanner;
+
+/**
+ * This program prompts the user to enter two numbers
+ * and then calculates and displays their product/multiplication.
+ */
+public class ProductOfTwoNumbers {
+
+    public static void main(String[] args) {
+
+        // 1. Create a Scanner object to read user input.
+        // This is necessary for the program to interact with the keyboard.
+        Scanner scanner = new Scanner(System.in);
+
+        // 2. Ask the user to enter the first number.
+        System.out.println("Please enter the first number:");
+
+        // 3. Read the first number entered by the user and store it
+        // in a 'double' type variable. We use 'double' to allow for
+        // numbers with decimals.
+        double number1 = scanner.nextDouble();
+
+        // 4. Ask the user to enter the second number.
+        System.out.println("Please enter the second number:");
+
+        // 5. Read the second number and store it in another variable.
+        double number2 = scanner.nextDouble()
+
+        // 6. Calculate the sum of the two numbers.
+        double sum = number1 + number2;
+
+        // 7. Display the product/multiplicqtion to the user.
+        // A descriptive message is used to make the result clear.
+        System.out.println("The product of " + number1 + " and " + number2 + " is: " + sum);
+
+        // 8. Close the Scanner object.
+        // This is a good practice to free up system resources.
+        scanner.close();
+    }
+}
